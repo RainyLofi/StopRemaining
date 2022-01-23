40d96782d98b783ab58bbf35378e2c60b87764d2
RainyLofi <71572372+RainyLofi@users.noreply.github.com>
2022-01-23 01:14:17 +0000

    headshotify

--- src/Modules/ClientOptimizer.lua
+++ src/Modules/ClientOptimizer.lua
@@ -76,22 +76,9 @@ end
 
 --------------------------------------------------------------------------------------------
 
-local Headshotify = function(Zombie, Damage)
-    warn('Headshotifying', Zombie.AI)
+-- auto headshot
+local Headshotify = function(Zombie)
     Zombie.Special = 'H'
-  --[[warn('1', Zombie.Special)
-    local Attachment = Workspace.Terrain:FindFirstChild('Attachment')
-    warn('2', Attachment)
-    local Pos = Attachment.Position
-    warn('3', Pos)
-    local Unit = (Attachment.Position - Pos).Unit
-    warn('4', Unit)
-
-    local v15 = 1
-    local v16 = 2.5
-    local v17 = 1.2
-
-    --Zombie.Velocity = Unit.Unit * ((Damage - Damage * v15 * 0.5) * v16 * v17) -- there is some kind of check, it's very weird]]--
 end
 
 local OldNameCall
@@ -103,15 +90,17 @@ OldNameCall = hookmetamethod(game, "__namecall", function(self, ...)
 
     if Method == 'FireServer' then
         if Args[1] == 'LL' then
-            --local WeaponModel, WeaponStats = Shared.Functions.GetWeaponModel()
-            --print(WeaponModel, WeaponStats)
-
             local Zombies = Args[2]
             for _, Zombie in pairs(Zombies) do
                 if not Zombie.Special or Zombie.Special ~= 'H' then
-                    if Zombie.AI.Name == 'Burster' or Zombie.AI.Name == 'Bloater' then
+                    if Zombie.AI.Name == 'Burster' or Zombie.AI.Name == 'Bloater' or Zombie.AI.Name == 'Riot' then
                         Headshotify(Zombie)
-                    elseif Zombie.AI.Name == 'Military' or Zombie.AI.Name == 'Riot' or Zombie.AI.Name == 'Hazmat' then
+                    elseif Zombie.AI.Name == 'Military' or Zombie.AI.Name == 'Hazmat' then
+                        local HeadshotChance = math.random(1, 2)
+                        if HeadshotChance == 1 then
+                            Headshotify(Zombie)
+                        end
+                    elseif Zombie.AI.Name == 'Sprinter' then
                         local HeadshotChance = math.random(1, 4)
                         if HeadshotChance == 1 then
                             Headshotify(Zombie)
@@ -129,97 +118,6 @@ OldNameCall = hookmetamethod(game, "__namecall", function(self, ...)
 
     return OldNameCall(self, unpack(Args))
 end)
---[[
-local Headshotify = function(Zombie, Damage)
-    warn('Headshotifying', Zombie.AI)
-    Zombie.Special = 'H'
-
-    local Attachment = Workspace.Terrain:FindFirstChild('Attachment')
-    local Pos = Attachment.Position
-    local Unit = (Attachment.Position - Pos).Unit
-
-    local v15 = 1
-    local v16 = 2.5
-    local v17 = 1.2
-
-    Zombie.Velocity = Unit.Unit * ((Damage - Damage * v15 * 0.5) * v16 * v17) -- there is some kind of check, it's very weird
-end
-
-local Hooked = function(original_function, self, ...)
-    local Args = {...}
-
-    if Args[1] == 'LL' then
-        warn('here3')
-        local WeaponModel, WeaponStats = Shared.Functions.GetWeaponModel()
-
-        local Zombies = Args[2]
-        for _, Zombie in pairs(Zombies) do
-            if not Zombie.Special or Zombie.Special ~= 'H' then
-                if Zombie.AI.Name == 'Burster' or Zombie.AI.Name == 'Bloater' then
-                    Headshotify(Zombie, WeaponStats.Damage)
-                elseif Zombie.AI.Name == 'Military' or Zombie.AI.Name == 'Riot' or Zombie.AI.Name == 'Hazmat' then
-                    local HeadshotChance = math.random(1, 4)
-                    if HeadshotChance == 1 then
-                        Headshotify(Zombie, WeaponStats.Damage)
-                    end
-                else
-                    local HeadshotChance = math.random(1, 8)
-                    if HeadshotChance == 1 then
-                        Headshotify(Zombie, WeaponStats.Damage)
-                    end
-                end
-            end
-        end
-
-        return original_function(self, unpack(Args))
-    end
-
-    return original_function(self, ...)
-end
-
-local metatable = getrawmetatable(game)
-
---// Custom functions aliases
-
-local setreadonly = setreadonly or set_readonly
-local make_writeable = make_writeable or function(t)
-	setreadonly(t, false)
-end
-local make_readonly = make_readonly or function(t)
-	setreadonly(t, true)
-end
-local protect_function = protect_function or newcclosureyield or newcclosure or function(...)
-	return ...
-end
-
-local detour_function = detour_function or replace_closure or hookfunction
-local get_namecall_method = get_namecall_method or getnamecallmethod
-
-local Methods = {
-	RemoteEvent = "FireServer",
-	RemoteFunction = "InvokeServer"
-}
-
-local Original = {}
-
-local function IsValidCall(Remote, Method, Arguments)
-	return Methods[Remote.ClassName] == Method
-end
-
-for Class, Method in next, Methods do --// FireServer and InvokeServer hooking ( FireServer(Remote, ...) )
-    local original_function = Instance.new(Class)[Method]
-    local function new_function(self, ...)
-        local Arguments = {...}
-        if typeof(self) == "Instance" and IsValidCall(self, Method, Arguments) then
-            return Hooked(original_function, self, Arguments)
-        end
-        return original_function(self, ...)
-    end
-    new_function = protect_function(new_function)
-    original_function = detour_function(original_function, new_function)
-    Original[new_function] = original_function
-    print("Hooked", Method)
-end]]--
 
 --------------------------------------------------------------------------------------------
 
